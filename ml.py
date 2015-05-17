c7d51e120337ccda64acc01d99de66b50fec8c75
jessewar <jesse.warren@live.com>
2015-05-17 15:31:42 -0700

    print m-estimates for both spam and ham

--- ml.py
+++ ml.py
@@ -1,7 +1,9 @@
 from collections import Counter
 
+# Get counts of words for spam and ham emails
 training_file = open('train', 'r')
 spam_word_counts = Counter()
+ham_word_counts = Counter()
 vocabulary = set()
 for line in training_file:
     tokens = line.split(' ')
@@ -12,14 +14,24 @@ for line in training_file:
         vocabulary.add(word)
         if classification == 'spam':
             spam_word_counts[word] += count
+        else:
+            ham_word_counts[word] += count
 
-m = len(vocabulary)
-n = sum(spam_word_counts.values())
-p = 1.0 / m
-word_probabilities = {}
-for word in spam_word_counts:
-    n_c = spam_word_counts[word]
-    word_probabilities[word] = (n_c + m * p) / (n + m)
-sorted_words = sorted(word_probabilities.items(), key=lambda x: x[1], reverse=True)
-for word, probability in sorted_words[:5]:
-    print 'word: ' + word + ', ' + 'probability: ' + str(probability)
+# Compute m-estimates for each word
+def print_m_estimates(word_counts):
+    m = len(vocabulary)
+    n = sum(word_counts.values())
+    p = 1.0 / m
+    word_probabilities = {}
+    for word in word_counts:
+        n_c = word_counts[word]
+        word_probabilities[word] = (n_c + m * p) / (n + m)
+    sorted_words = sorted(word_probabilities.items(), key=lambda x: x[1], reverse=True)
+    for word, probability in sorted_words[:5]:
+        print 'word: ' + word + ', ' + 'probability: ' + str(probability)
+
+
+print("spam------------")
+print_m_estimates(spam_word_counts)
+print("ham-------------")
+print_m_estimates(ham_word_counts)
