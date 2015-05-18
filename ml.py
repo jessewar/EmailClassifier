8bce814ffd9ecd8d7fcd11aa746bacf3a76581a3
jessewar <jesse.warren@live.com>
2015-05-18 15:20:53 -0700

    0.509 correct classifications

--- ml.py
+++ ml.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import math
 
 # Get prior probabilities of spam and ham
 ham_count = 0.0
@@ -13,9 +14,6 @@ for line in training_file:
         ham_count += 1
     else:
         spam_count += 1
-
-ham_prior = ham_count / total_count
-spam_prior = spam_count / total_count
 
 # Get counts of words for spam and ham emails
 training_file = open('train', 'r')
@@ -39,7 +37,7 @@ def get_word_likelihoods(word_counts):
     m = len(vocabulary)
     n = sum(word_counts.values())
     p = 1.0 / m
-    word_probabilities = {}
+    word_probabilities = Counter()
     for word in word_counts:
         n_c = word_counts[word]
         word_probabilities[word] = (n_c + m * p) / (n + m)
@@ -51,11 +49,44 @@ def top_five_words(word_likelihoods):
     for word, probability in sorted_words[:5]:
         print 'word: ' + word + ', ' + 'probability: ' + str(probability)
 
-
+# Classify new data
+ham_prior = ham_count / total_count
+spam_prior = spam_count / total_count
 spam_likelihoods = get_word_likelihoods(spam_word_counts)
-ham_likelihoods = get_word_likelihoods(ham_word_counts)
-print("spam------------")
-top_five_words(spam_likelihoods)
-print("ham-------------")
-top_five_words(ham_likelihoods)
+ham_likelihoods = get_word_likelihoods(ham_word_counts)        
 
+def classify_example(word_counts):
+    spam_probability = spam_prior
+    ham_probability = ham_prior
+    for word, count in word_counts.items():
+        for i in range(count):
+            spam_probability *= math.log(spam_likelihoods[word] if spam_likelihoods[word] > 0 else 1)
+            ham_probability *= math.log(ham_likelihoods[word] if ham_likelihoods[word] > 0 else 1)
+    if spam_probability == 0 and ham_probability == 0:
+        return None
+    return 'spam' if spam_probability > ham_probability else 'ham'
+
+none_count = 0
+correct_count = 0.0
+total_count = 0.0
+test_file = open('test', 'r')
+for line in test_file:
+    word_counts = Counter()
+    tokens = line.split(' ')
+    actual_classification = tokens[1]
+    for i in range(2, len(tokens), 2):
+        word = tokens[i]
+        count = int(tokens[i+1])
+        word_counts[word] += count
+    predicted_classification = classify_example(word_counts)
+    if predicted_classification is None:
+        none_count += 1
+    if predicted_classification == actual_classification:
+        correct_count += 1
+    total_count += 1
+    # if total_count > 5:
+    #     break
+
+print 'Percentage correct: ' + str(correct_count / total_count)
+    
+print none_count
