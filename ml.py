41e800796be1fcc3e71981851454b5e1005cf009
jessewar <jesse.warren@live.com>
2015-05-22 09:35:19 -0700

    cleaned up

--- ml.py
+++ ml.py
@@ -8,18 +8,15 @@ def top_five_words(word_likelihoods):
         print 'word: ' + word + ', ' + 'probability: ' + str(probability)
 
 # Compute m-estimates for each word
-def get_word_likelihoods(word_counts):
-    m = len(vocabulary)
-    n = sum(word_counts.values())
-    p = 1.0 / m
-    word_probabilities = Counter()
+def get_word_likelihoods(word_counts, m, n, p):
+    word_likelihoods = Counter()
     for word in word_counts:
         n_c = word_counts[word]
-        word_probabilities[word] = (n_c + m * p) / (n + m)
-    return word_probabilities
+        word_likelihoods[word] = (n_c + m * p) / (n + m)
+    return word_likelihoods
 
 # Classifies a new email as 'spam' or 'ham' based on its word counts
-def classify_example(word_counts):
+def classify_example(word_counts, spam_prior, ham_prior, spam_likelihoods, ham_likelihoods):
     spam_probability = spam_prior
     ham_probability = ham_prior
     for word, count in word_counts.items():
@@ -29,27 +26,24 @@ def classify_example(word_counts):
     return 'spam' if spam_probability > ham_probability else 'ham'
 
 # Get prior probabilities of spam and ham
+# Get counts of words for spam and ham emails
+training_file = open('train', 'r')
 ham_count = 0.0
 spam_count = 0.0
 total_count = 0.0
-training_file = open('train', 'r')
-for line in training_file:
-    tokens = line.split(' ')
-    classification = tokens[1]
-    total_count += 1
-    if classification == 'ham':
-        ham_count += 1
-    else:
-        spam_count += 1
-
-# Get counts of words for spam and ham emails
-training_file = open('train', 'r')
 spam_word_counts = Counter()
 ham_word_counts = Counter()
 vocabulary = set()
 for line in training_file:
     tokens = line.split(' ')
     classification = tokens[1]
+    # update spam/ham email counts
+    total_count += 1
+    if classification == 'spam':
+        spam_count += 1
+    else:
+        ham_count += 1
+    # update spam/ham word counts
     for i in range(2, len(tokens), 2):
         word = tokens[i]
         count = int(tokens[i+1])
@@ -62,10 +56,8 @@ for line in training_file:
 # Classify new data
 ham_prior = ham_count / total_count
 spam_prior = spam_count / total_count
-print ham_prior
-print spam_prior
-spam_likelihoods = get_word_likelihoods(spam_word_counts)
-ham_likelihoods = get_word_likelihoods(ham_word_counts)        
+spam_likelihoods = get_word_likelihoods(spam_word_counts, len(vocabulary), sum(spam_word_counts.values()), 1.0 / len(vocabulary))
+ham_likelihoods = get_word_likelihoods(ham_word_counts, len(vocabulary), sum(ham_word_counts.values()), 1.0 / len(vocabulary))        
 
 correct_count = 0.0
 total_count = 0.0
@@ -78,7 +70,7 @@ for line in test_file:
         word = tokens[i]
         count = int(tokens[i+1])
         word_counts[word] += count
-    predicted_classification = classify_example(word_counts)
+    predicted_classification = classify_example(word_counts, spam_prior, ham_prior, spam_likelihoods, ham_likelihoods)
     if predicted_classification == actual_classification:
         correct_count += 1
     total_count += 1
