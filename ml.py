442d25affa9291a6f85ad984ae7ef955e7dd218a
jessewar <jesse.warren@live.com>
2015-05-22 09:10:36 -0700

    .51 success, debugging

--- ml.py
+++ ml.py
@@ -1,6 +1,33 @@
 from collections import Counter
 import math
 
+# Print the top five words with the highest likelihoods of occurring
+def top_five_words(word_likelihoods):
+    sorted_words = sorted(word_likelihoods.items(), key=lambda x: x[1], reverse=True)
+    for word, probability in sorted_words[:5]:
+        print 'word: ' + word + ', ' + 'probability: ' + str(probability)
+
+# Compute m-estimates for each word
+def get_word_likelihoods(word_counts):
+    m = len(vocabulary)
+    n = sum(word_counts.values())
+    p = 1.0 / m
+    word_probabilities = Counter()
+    for word in word_counts:
+        n_c = word_counts[word]
+        word_probabilities[word] = (n_c + m * p) / (n + m)
+    return word_probabilities
+
+# Classifies a new email as 'spam' or 'ham' based on its word counts
+def classify_example(word_counts):
+    spam_probability = spam_prior
+    ham_probability = ham_prior
+    for word, count in word_counts.items():
+        for i in range(count):
+            spam_probability *= math.log(spam_likelihoods[word]) if spam_likelihoods[word] > 0 else 1
+            ham_probability *= math.log(ham_likelihoods[word]) if ham_likelihoods[word] > 0 else 1
+    return 'spam' if spam_probability > ham_probability else 'ham'
+
 # Get prior probabilities of spam and ham
 ham_count = 0.0
 spam_count = 0.0
@@ -32,37 +59,13 @@ for line in training_file:
         else:
             ham_word_counts[word] += count
 
-# Compute m-estimates for each word
-def get_word_likelihoods(word_counts):
-    m = len(vocabulary)
-    n = sum(word_counts.values())
-    p = 1.0 / m
-    word_probabilities = Counter()
-    for word in word_counts:
-        n_c = word_counts[word]
-        word_probabilities[word] = (n_c + m * p) / (n + m)
-    return word_probabilities
-
-# Print the top five words with the highest likelihoods of occurring
-def top_five_words(word_likelihoods):
-    sorted_words = sorted(word_likelihoods.items(), key=lambda x: x[1], reverse=True)
-    for word, probability in sorted_words[:5]:
-        print 'word: ' + word + ', ' + 'probability: ' + str(probability)
-
 # Classify new data
 ham_prior = ham_count / total_count
 spam_prior = spam_count / total_count
+print ham_prior
+print spam_prior
 spam_likelihoods = get_word_likelihoods(spam_word_counts)
 ham_likelihoods = get_word_likelihoods(ham_word_counts)        
-
-def classify_example(word_counts):
-    spam_probability = spam_prior
-    ham_probability = ham_prior
-    for word, count in word_counts.items():
-        for i in range(count):
-            spam_probability *= math.log(spam_likelihoods[word]) if spam_likelihoods[word] > 0 else 1
-            ham_probability *= math.log(ham_likelihoods[word]) if ham_likelihoods[word] > 0 else 1
-    return 'spam' if spam_probability > ham_probability else 'ham'
 
 correct_count = 0.0
 total_count = 0.0
@@ -76,8 +79,6 @@ for line in test_file:
         count = int(tokens[i+1])
         word_counts[word] += count
     predicted_classification = classify_example(word_counts)
-    if predicted_classification is None:
-        none_count += 1
     if predicted_classification == actual_classification:
         correct_count += 1
     total_count += 1
